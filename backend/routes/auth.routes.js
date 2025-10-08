5dab7fc07804eea190d350d8e980bea25d8177f9
Aman Yadav <amandotyadav@gmail.com>
2025-10-08 15:43:37 +0530

    Fixed bug in auth routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { login, logout, register, sendVerifyOTP, verifyEmail } from "../controllers/auth.controller.js";
+import {
+  isAuthenticated,
+  login,
+  logout,
+  register,
+  sendVerifyOTP,
+  verifyEmail,
+} from "../controllers/auth.controller.js";
 import userAuth from "../middlewares/auth.middleware.js";
 
 const authRouter = express.Router();
@@ -9,6 +16,6 @@ authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 authRouter.post("/send-verify-otp", userAuth, sendVerifyOTP);
 authRouter.post("/verify-account", userAuth, verifyEmail);
-authRouter.post("/isAuth", userAuth, verifyEmail);
+authRouter.post("/isAuth", userAuth, isAuthenticated);
 
 export default authRouter;
